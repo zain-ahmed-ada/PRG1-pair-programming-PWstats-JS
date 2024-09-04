a3c42c24d71e9135839db2f60111018cb3a55659
Steve Rich <steven.john.rich@gmail.com>
2024-09-04 15:06:28 +0100

    Update stats.js

--- stats.js
+++ stats.js
@@ -4,16 +4,22 @@ const inputFile = "10000-most-common-passwords.csv";
 const outputFile = "statistics.csv";
 const delimiter = ",";
 
-if (fs.existsSync(outputFile)) { // check for existing output file
-  fs.unlinkSync(outputFile); // delete it true. (To start a fresh file)
+function deleteExistingOutputFile() {
+  if (fs.existsSync(outputFile)) {
+    fs.unlinkSync(outputFile);
+  }
 }
 
-const data = fs.readFileSync(inputFile, "utf-8");
-const lines = data.split(/\n/);
+function processData() {
+  const data = fs.readFileSync(inputFile, "utf-8");
+  const lines = data.split(/\n/);
 
-for (line of lines) {
-      
-      elements = line.split(delimiter);
-      console.log(elements)
+  for (line of lines) {
+    elements = line.split(delimiter);
+    console.log(elements);
+  }
 }
 
+// Main execution
+deleteExistingOutputFile(); 
+processData();
