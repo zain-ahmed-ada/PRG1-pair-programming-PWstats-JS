f54332a47df865d523ba6ee1ae42767b37635924
zain-ahmed-ada <zain.ahmed@ada.ac.uk>
2024-10-28 21:30:25 +0000

    Completed task

--- stats.js
+++ stats.js
@@ -5,21 +5,38 @@ const outputFile = "statistics.csv";
 const delimiter = ",";
 
 function deleteExistingOutputFile() {
-  if (fs.existsSync(outputFile)) {
-    fs.unlinkSync(outputFile);
-  }
+	if (fs.existsSync(outputFile)) {
+		fs.unlinkSync(outputFile);
+	}
 }
 
 function processData() {
-  const data = fs.readFileSync(inputFile, "utf-8");
-  const lines = data.split(/\n/);
+	const data = fs.readFileSync(inputFile, "utf-8");
+	const lines = data.split(/\n/);
 
-  for (let line of lines) {
-    elements = line.split(delimiter);
-    console.log(elements);
-  }
+	const countList = [];
+
+	for (let line of lines) {
+		let elements = line.split(delimiter);
+		let character = elements[1].length;
+		let newCharacter = character.toString();
+
+		const existingEntry = countList.find((entry) => entry.Char === newCharacter);
+		if (existingEntry) {
+			existingEntry.Counter += 1;
+		} else {
+			countList.push({
+				Char: newCharacter,
+				Counter: 1,
+			});
+		}
+	}
+
+	let newData = countList.map((entry) => `Char: ${entry.Char}, Count: ${entry.Counter}`).join("\n");
+
+	fs.appendFileSync(outputFile, newData);
 }
 
 // Main execution
-deleteExistingOutputFile(); 
+deleteExistingOutputFile();
 processData();
